56516dd12ca18ff33886c7430b1db73014059572
Kristine Joy Martinez <kjvmartinez@Kristines-MacBook-Air-5.local>
2022-05-15 11:37:28 +0800

    add list_display and search_fields in admin

--- artgallery/projectsite/admin.py
+++ artgallery/projectsite/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Art
 # Register your models here.
 class ArtAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "size", "style", "painter", "yearofcreation", "artdescription")
+    search_fields = ("name", "painter")
 
 admin.site.register(Art, ArtAdmin)
